01759e1abb136f9bba40584b0a7c65455509a151
Edo-Vy <tranthaovy8898@gmai.com>
2023-07-21 11:08:49 +0700

    js-validation-date

--- controllers/index.js
+++ controllers/index.js
@@ -3,33 +3,31 @@
 
 // Bài tập quản lý sinh viên
 
-var mangSinhVien = [];// [sv1, sv2 ] -> [{maSV: 01, tenSV: nguyen van a, },{maSV: 02, tenSV: nguyen van b, }]
+var mangSinhVien = []; // [sv1, sv2 ] -> [{maSV: 01, tenSV: nguyen van a, },{maSV: 02, tenSV: nguyen van b, }]
 //input : thông tin sinh viên : SinhVien
 
-document.querySelector('#btnThemSV').onclick = function () {
-
+document.querySelector("#btnThemSV").onclick = function () {
     // var mangSinhVien = []; // [sv1, sv2 ] -> [{maSV: 01, tenSV: nguyen van a, },{maSV: 02, tenSV: nguyen van b, }]
     //input : thông tin sinh viên : SinhVien
     var sv = new SinhVien();
     // Lấy thông tin từ giao diện đưa vào input sv
-    sv.maSV = document.querySelector('#maSV').value;
-    sv['tenSV'] = document.querySelector('#tenSV').value;
-    sv.emailSV = document.querySelector('#emailSV').value;
-    sv.matKhau = document.querySelector('#matKhau').value;
+    sv.maSV = document.querySelector("#maSV").value;
+    sv["tenSV"] = document.querySelector("#tenSV").value;
+    sv.emailSV = document.querySelector("#emailSV").value;
+    sv.matKhau = document.querySelector("#matKhau").value;
 
     // Ngày Sinh -> format client
-    var ngaySinh = new Date(document.querySelector('#ngaySinh').value);
+    var ngaySinh = new Date(document.querySelector("#ngaySinh").value);
     sv.ngaySinh = ngaySinh.toLocaleDateString();
 
-    sv.khoaHoc = document.querySelector('#khoaHoc').value;
-    sv.diemToan = document.querySelector('#diemToan').value;
-    sv.diemLy = document.querySelector('#diemLy').value;
-    sv.diemHoa = document.querySelector('#diemHoa').value;
+    sv.khoaHoc = document.querySelector("#khoaHoc").value;
+    sv.diemToan = document.querySelector("#diemToan").value;
+    sv.diemLy = document.querySelector("#diemLy").value;
+    sv.diemHoa = document.querySelector("#diemHoa").value;
 
     console.log(sv);
+
     //output : html  String <tr></tr>
-
-
 
     // Bắt lỗi khi người dùng nhập không hợp lệ
     // .trim() : Loại bỏ khoảng trống đầu và cuối của 1 chuỗi => VD :     abc     => abc
@@ -38,16 +36,36 @@ document.querySelector('#btnThemSV').onclick = function () {
     //     alert('Mã sinh viên không hợp lệ');
     //     return;
     // }
-    // Kiểm tra rỗng
-    var valid = true;
-    valid &= kiemTraRong(sv.maSV,'#required__err__ma', 'Mã Sinh Viên') & kiemTraRong(sv.tenSV,'#required__err__ten', 'Tên Sinh Viên');
 
-    valid &= kiemTraTatCaKyTu(sv.tenSV,'#all__letter', 'Tên Sinh Viên');
+    var valid = true;
+    // Kiểm tra độ dài
+    valid &= kiemTraDoDai(sv.matKhau, "#err__matkhau", " Mật Khẩu", 6, 32);
+    // Kiểm tra rỗng
+    valid &=
+        kiemTraRong(sv.maSV, "#required__err__ma", "Mã Sinh Viên") &
+        kiemTraRong(sv.tenSV, "#required__err__ten", "Tên Sinh Viên");
+    // Kiểm tra định dạng
+    var ngaySinhTest = moment(ngaySinh).format("YYYY-MM-DD");
+    
+    valid &=
+        kiemTraTatCaKyTu(sv.tenSV, "#all__letter", "Tên Sinh Viên") &
+        kiemTraTatCaLaSo(sv.diemToan, "#err_diemToan", "Điểm Toán") &
+        kiemTraTatCaLaSo(sv.diemLy, "#err_diemLy", "Điểm lý") &
+        kiemTraTatCaLaSo(sv.diemHoa, "#err_diemHoa", "Điểm Hóa") &
+        kiemTraEmail(sv.emailSV, "#err__mail", "Email") &
+        kiemTraMatKhau(sv.matKhau, "#err__matkhau", "Mật Khẩu") &
+        kiemTraNgayThangNam(ngaySinhTest, "#err_date", "Ngày Sinh");
+    // Kiểm tra giá trị
+    valid &=
+        kiemTraValue(sv.diemToan, "#err_diem_max_min_toan", "Điểm Toán", 0, 10) &
+        kiemTraValue(sv.diemLy, "#err_diem_max_min_ly", "Điểm Lý", 0, 10) &
+        kiemTraValue(sv.diemHoa, "#err_diem_max_min_hoa", "Điểm Hóa", 0, 10);
     // Kiểm tra biến cờ
 
-    if(!valid){
+    if (!valid) {
         return;
     }
+
     // Cách 1: Tạo html = createElement
     {
         // // - Bước 1: Tạo ra thẻ tr
@@ -55,28 +73,21 @@ document.querySelector('#btnThemSV').onclick = function () {
         // // Tạo ra td mã sinh viên
         // var tdMaSinhVien = document.createElement('td');
         // tdMaSinhVien.innerHTML = sv.maSV;
-
         // var tdTenSinhVien = document.createElement('td');
         // tdTenSinhVien.innerHTML = sv.tenSV;
-
         // var tdEmailSinhVien = document.createElement('td');
         // tdEmailSinhVien.innerHTML = sv.emailSV;
-
         // var tdNgaySinhSinhVien = document.createElement('td');
         // tdNgaySinhSinhVien.innerHTML = sv.ngaySinh;
-
         // var tdKhoaHoc = document.createElement('td');
         // tdKhoaHoc.innerHTML = sv.khoaHoc;
-
         // var tdDTB = document.createElement('td');
         // tdDTB.innerHTML = sv.tinhDiemTrungBinh();
-
         // // Td chức năng
         // var btnXoa = document.createElement('button');
         // btnXoa.innerHTML = 'Xóa';
         // btnXoa.className = 'btn btn-danger';
         // btnXoa.onclick = function(){
-
         //     // từ thẻ con dom ra thẻ cha .td -> .tr
         //     // var trParent = btnXoa.parentElement.parentElement;
         //    // closest : dom đến selector gần nhất chứa nó
@@ -84,11 +95,8 @@ document.querySelector('#btnThemSV').onclick = function () {
         //     // tdParent.remove();
         //     trParent.remove();
         // }
-
         // var tdChucNang = document.createElement('td');
         // tdChucNang.appendChild(btnXoa);
-
-
         // // Thêm nội dung td vào tr
         // trSinhVien.appendChild(tdMaSinhVien);
         // trSinhVien.appendChild(tdTenSinhVien);
@@ -98,7 +106,6 @@ document.querySelector('#btnThemSV').onclick = function () {
         // trSinhVien.appendChild(tdDTB);
         // //Thêm td chức năng
         // trSinhVien.appendChild(tdChucNang);
-
         // // Thêm nội dung tr vào giao diện
         // var tBody = document.querySelector('#tblSinhVien');
         // tBody.appendChild(trSinhVien);
@@ -108,41 +115,38 @@ document.querySelector('#btnThemSV').onclick = function () {
 
     mangSinhVien.push(sv);
 
-    console.log('Mảng Sinh Viên', mangSinhVien);
+    console.log("Mảng Sinh Viên", mangSinhVien);
     // Sau khi thêm 1 sinh viên => mảng sinh viên [{},{},...]
     renderTableSinhVien(mangSinhVien);
 
-
     // Sau khi thêm sinh viên thành công => lưu mảng sinh viên vào localStorage
     var sSinhVien = JSON.stringify(mangSinhVien);
-    luulocalStorage('mangSinhVien', sSinhVien);
+    luulocalStorage("mangSinhVien", sSinhVien);
+};
 
-}
-
-function renderTableSinhVien(arrSinhVien) { // input :arrSinhVien [{},{},..]
-
+function renderTableSinhVien(arrSinhVien) {
+    // input :arrSinhVien [{},{},..]
 
     //output html = <tr><td></td>.....</tr>
-    var htmlContent = '';
+    var htmlContent = "";
     // Duyệt qua các object của mảng sinh viên
     for (var index = 0; index < arrSinhVien.length; index++) {
-
         var sv = arrSinhVien[index]; // Mỗi lần duyệt qua lấy ra 1 object thứ index của arrSinhVien [{0}, {1}]
 
         // Nếu bấm từ nút thêm ( được new từ SinhVien => nên sẽ có tinhDiemTrungBinh)
         // Nếu lấy từ localStorage thì bị mất phương thức tínhDiemTrungBinh
         // hasOwnProperty('ten_thuoc_tinh') : Nếu có tên thuộc tính đó trong object thì trả về giá trị true, không có trả về false
-        if (!sv.hasOwnProperty('tinhDiemTrungBinh')) {
-
+        if (!sv.hasOwnProperty("tinhDiemTrungBinh")) {
             sv.__proto__.tinhDiemTrungBinh = function () {
                 //.__proto__. : mở rộng thuộc tính của object
                 var diemTB = 0;
-                diemTB = (Number(sv.diemToan) + Number(sv.diemHoa) + Number(sv.diemLy)) / 3;
+                diemTB =
+                    (Number(sv.diemToan) + Number(sv.diemHoa) + Number(sv.diemLy)) / 3;
                 return diemTB;
-            }
+            };
         }
         // Từ object tạo ra thẻ tr
-        //  `` -> String template : gõ xuống dòng 
+        //  `` -> String template : gõ xuống dòng
 
         var tr = `
              <tr>
@@ -154,8 +158,10 @@ function renderTableSinhVien(arrSinhVien) { // input :arrSinhVien [{},{},..]
                 <td>${sv.tinhDiemTrungBinh()}</td>
                 <td>
                     <button class ="btn btn-danger" onclick ="xoaSinhVien('${index}')">  Xóa </button>
-                    <button class ="btn btn-primary" onclick ="suaSinhVien('${sv.maSV}')"> Sửa </button>
-                    <button class ="btn btn-danger ml-2" onclick ="xoaMaSinhVien('${sv.maSV}')">  Xóa Mã Sinh Viên </button>
+                    <button class ="btn btn-primary" onclick ="suaSinhVien('${sv.maSV
+            }')"> Sửa </button>
+                    <button class ="btn btn-danger ml-2" onclick ="xoaMaSinhVien('${sv.maSV
+            }')">  Xóa Mã Sinh Viên </button>
                    
                 </td>
              </tr>
@@ -163,15 +169,11 @@ function renderTableSinhVien(arrSinhVien) { // input :arrSinhVien [{},{},..]
         // Mỗi lần tạo xong thẻ tr sẽ + vào output
 
         htmlContent += tr;
-
     }
-    document.querySelector('#tblSinhVien').innerHTML = htmlContent;
-
-
+    document.querySelector("#tblSinhVien").innerHTML = htmlContent;
 }
 
 function xoaSinhVien(index) {
-
     mangSinhVien.splice(index, 1);
 
     // Sau khi xóa 1 sinh viên thì tạo lại bảng
@@ -200,18 +202,16 @@ function xoaSinhVien(index) {
 // Xóa  mã sinh viên bị trùng
 
 function xoaMaSinhVien(maSinhVien) {
-
     var viTriXoa = -1;
     for (var index = mangSinhVien.length - 1; index >= 0; index--) {
         // Mỗi lần duyệt lấy ra 1 sinh viên
         var sv = mangSinhVien[index];
-        if (sv.maSV == maSinhVien) { // Nếu obj sinh viên trong mảng == mã sinh viên được click thì lấy ra vị trí
+        if (sv.maSV == maSinhVien) {
+            // Nếu obj sinh viên trong mảng == mã sinh viên được click thì lấy ra vị trí
 
             mangSinhVien.splice(viTriXoa, 1);
         }
-
     }
-
 
     // Sau khi xóa tạo lại table mới
     renderTableSinhVien(mangSinhVien);
@@ -219,7 +219,6 @@ function xoaMaSinhVien(maSinhVien) {
 
 // Lưu thông tin localStorage
 function luulocalStorage(key, value) {
-
     localStorage.setItem(key, value);
 }
 
@@ -235,16 +234,14 @@ function laylocalStorage(key) {
 
 // Định nghĩa sự kiện khi trang load xong html
 window.onload = function () {
-
-    var value = laylocalStorage('mangSinhVien');
+    var value = laylocalStorage("mangSinhVien");
     if (value !== undefined) {
         // Biển đổi value thành mảng lại
         mangSinhVien = JSON.parse(value);
         // Gọi hàm từ mảng để tạo ra table
         renderTableSinhVien(mangSinhVien);
     }
-}
-
+};
 
 //======== Mở rộng thuộc tính của Prototype ==================
 
@@ -260,7 +257,6 @@ window.onload = function () {
 
 // Sửa thông tin sinh viên
 function suaSinhVien(maSinhVienClick) {
-
     for (var index = 0; index < mangSinhVien.length; index++) {
         // Mỗi lần duyệt lấy ra 1 sinh viên object
         var sinhVien = mangSinhVien[index];
@@ -269,19 +265,18 @@ function suaSinhVien(maSinhVienClick) {
             // Tìm thấy
             // Gán các giá trị từ object lên các thẻ input
 
-            document.querySelector('#maSV').value = sinhVien.maSV;
-            document.querySelector('#tenSV').value = sinhVien['tenSV'];
-            document.querySelector('#emailSV').value = sinhVien.emailSV;
-            document.querySelector('#matKhau').value = sinhVien.matKhau;
+            document.querySelector("#maSV").value = sinhVien.maSV;
+            document.querySelector("#tenSV").value = sinhVien["tenSV"];
+            document.querySelector("#emailSV").value = sinhVien.emailSV;
+            document.querySelector("#matKhau").value = sinhVien.matKhau;
 
-            var value = moment(sinhVien.ngaySinh).format('YYYY-MM-DD');
-            document.querySelector('#ngaySinh').value = value;
+            var value = moment(sinhVien.ngaySinh).format("YYYY-MM-DD");
+            document.querySelector("#ngaySinh").value = value;
 
-            document.querySelector('#khoaHoc').value = sinhVien.khoaHoc;
-            document.querySelector('#diemToan').value = sinhVien.diemToan;
-            document.querySelector('#diemLy').value = sinhVien.diemLy;
-            document.querySelector('#diemHoa').value = sinhVien.diemHoa;
-
+            document.querySelector("#khoaHoc").value = sinhVien.khoaHoc;
+            document.querySelector("#diemToan").value = sinhVien.diemToan;
+            document.querySelector("#diemLy").value = sinhVien.diemLy;
+            document.querySelector("#diemHoa").value = sinhVien.diemHoa;
 
             break;
         }
@@ -290,29 +285,27 @@ function suaSinhVien(maSinhVienClick) {
 
 // Cập nhật
 
-
-document.querySelector('#btnCapNhatSV').onclick = function () {
+document.querySelector("#btnCapNhatSV").onclick = function () {
     // Lấy dữ liệu từ người dùng nhập vào sau khi chỉnh sửa
 
     var svCapNhat = new SinhVien();
     // Lấy thông tin từ giao diện đưa vào input sv
-    svCapNhat.maSV = document.querySelector('#maSV').value;
-    svCapNhat['tenSV'] = document.querySelector('#tenSV').value;
-    svCapNhat.emailSV = document.querySelector('#emailSV').value;
-    svCapNhat.matKhau = document.querySelector('#matKhau').value;
+    svCapNhat.maSV = document.querySelector("#maSV").value;
+    svCapNhat["tenSV"] = document.querySelector("#tenSV").value;
+    svCapNhat.emailSV = document.querySelector("#emailSV").value;
+    svCapNhat.matKhau = document.querySelector("#matKhau").value;
 
     // Ngày Sinh -> format client
-    var ngaySinh = new Date(document.querySelector('#ngaySinh').value);
+    var ngaySinh = new Date(document.querySelector("#ngaySinh").value);
     svCapNhat.ngaySinh = ngaySinh.toLocaleDateString();
 
-    svCapNhat.khoaHoc = document.querySelector('#khoaHoc').value;
-    svCapNhat.diemToan = document.querySelector('#diemToan').value;
-    svCapNhat.diemLy = document.querySelector('#diemLy').value;
-    svCapNhat.diemHoa = document.querySelector('#diemHoa').value;
+    svCapNhat.khoaHoc = document.querySelector("#khoaHoc").value;
+    svCapNhat.diemToan = document.querySelector("#diemToan").value;
+    svCapNhat.diemLy = document.querySelector("#diemLy").value;
+    svCapNhat.diemHoa = document.querySelector("#diemHoa").value;
 
     // Duyệt qua mảng tìm ra object sinhvien cần cập nhật
     for (var index = 0; index < mangSinhVien.length; index++) {
-
         // Mỗi lần duyệt lấy ra 1 sv trong mảng
         var svMang = mangSinhVien[index];
         if (svMang.maSV === svCapNhat.maSV) {
@@ -330,5 +323,4 @@ document.querySelector('#btnCapNhatSV').onclick = function () {
             break;
         }
     }
-
-}
+};
--- util/validation.js
+++ util/validation.js
@@ -1,9 +1,9 @@
 // Hàm kiểm tra 
 
-function kiemTraRong(value,selectorError, name){
+function kiemTraRong(value, selectorError, name) {
 
     // .trim() : loại bỏ khoảng trống
-    if(value.trim() === ''){
+    if (value.trim() === '') {
 
         document.querySelector(selectorError).innerHTML = name + ' không hợp lệ! ';
         return false;
@@ -15,10 +15,10 @@ function kiemTraRong(value,selectorError, name){
 }
 
 
-function kiemTraTatCaKyTu(value,selectorError, name){
+function kiemTraTatCaKyTu(value, selectorError, name) {
 
     var regexLetter = /^[A-Z a-z]+$/;
-    if ( regexLetter.test(value)){
+    if (regexLetter.test(value)) {
 
         document.querySelector(selectorError).innerHTML = '';
         return true;
@@ -27,4 +27,81 @@ function kiemTraTatCaKyTu(value,selectorError, name){
     document.querySelector(selectorError).innerHTML = name + ' tất cả phải là ký tự!';
     return false;
 
+}
+
+function kiemTraTatCaLaSo(value, selectorError, name) {
+
+    var regexAllNumber = /^[0-9]+$/;
+    if (regexAllNumber.test(value)) {
+        document.querySelector(selectorError).innerHTML = '';
+        return true;
+    }
+    document.querySelector(selectorError).innerHTML = name + ' tất cả phải là số!';
+    return false;
+}
+
+function kiemTraEmail(value, selectorError, name) {
+
+    var regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (regexEmail.test(value)) {
+
+        document.querySelector(selectorError).innerHTML = '';
+        return true;
+    }
+    document.querySelector(selectorError).innerHTML = name + ' không hợp lệ!';
+    return false;
+}
+
+function kiemTraMatKhau(value, selectorError, name) {
+
+    // PassWord phải có số và chữ, ít nhất 1 kí tự viết hoa
+    var regexPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{0,}$/;
+    if (regexPass.test(value)) {
+
+        document.querySelector(selectorError).innerHTML = '';
+        return true;
+    }
+
+    document.querySelector(selectorError).innerHTML = name + 'phải có số và chữ, ít nhất 1 ký tự viết hoa và 1 ký tự đặc biệt, từ 6 đến 32 ký tự';
+    return false;
+}
+
+function kiemTraDoDai(value,selectorError,name,minLength, maxLength){
+     if(value.length > value.maxLength || value.length < value.minLength){
+
+        document.querySelector(selectorError).innerHTML = name + ' độ dài từ ' + minLength + ' đến ' + maxLength;
+        return false;
+     }
+
+     document.querySelector(selectorError).innerHTML = '';
+     return true;
+}
+
+// kiểm tra giá trị min - max
+
+function kiemTraValue (value,selectorError,name,minValue, maxValue){
+
+    if(Number(value) < minValue || Number(value) > maxValue){
+
+        document.querySelector(selectorError).innerHTML = name + ' giá trị từ ' + minValue + ' đến ' + maxValue;
+        return false;
+    }
+    document.querySelector(selectorError).innerHTML = '';
+     return true;
+}
+
+// Kiểm tra Ngày - Tháng - Năm
+
+function kiemTraNgayThangNam (value,selectorError,name){
+
+    // yyyy-mm-dd
+    var regexDate = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
+    if(regexDate.test(value)){
+
+        document.querySelector(selectorError).innerHTML = '';
+        return true;
+    }
+
+    document.querySelector(selectorError).innerHTML = name + ' phải có định dạng yyyy-mm-dd';
+    return false;
 }
